ee78accb19fc941ff1ace1c48e0ce1c6a4a23b4d
Godkiller <daniel29348679@gmail.com>
2024-10-12 20:10:57 +0800

    winifh sheet 1

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,21 @@
-use axum::{routing::get, Form, Router};
+use axum::{routing::get, routing::post, Form, Router};
+use mysql::prelude::*;
+use mysql::*;
 use serde::Deserialize;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-type DataMap = Arc<Mutex<HashMap<String, String>>>;
 
 #[tokio::main]
 async fn main() {
     // Create a shared data map wrapped in Arc and Mutex
-    let data_map: DataMap = Arc::new(Mutex::new(HashMap::new()));
-
-    // Insert some initial data
-    {
-        let mut map = data_map.lock().expect("Failed to lock data_map");
-        map.insert("key1".to_string(), "value1".to_string());
-    }
 
     let app = Router::new()
         .route("/", get(root))
-        .route(
-            "/push",
-            axum::routing::post({
-                let data_map = data_map.clone();
-                move |iname: Form<Postsheet>| push_data(data_map.clone(), iname)
-            }),
-        )
-        .route(
-            "/list",
-            get({
-                let data_map = data_map.clone();
-                move || list_data(data_map.clone())
-            }),
-        );
+        .route("/list_all_worker_name", get(list_all_worker_name))
+        .route("/add_worker_name", post(add_worker_name))
+        .route("/remove_worker_name", post(remove_worker_name))
+        .route("/add_mission", post(add_mission))
+        .route("/remove_mission", post(remove_mission))
+        .route("/list_all_mission", post(list_all_mission))
+        .route("/update_mission_state", post(update_mission_state));
 
     // Run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -44,20 +29,197 @@ async fn root() -> String {
 
 #[derive(Deserialize)]
 struct Postsheet {
-    name: Option<String>,
-    missing: Option<String>,
-}
-async fn push_data(data_map: DataMap, Form(iname): Form<Postsheet>) -> String {
-    let mut map = data_map.lock().unwrap();
-    map.insert("key2".to_string(), iname.name.clone().unwrap());
-    "data pushed".to_string()
+    worker_name: Option<String>,
+    mission_name: Option<String>,
+    mission_id: Option<i32>,
+    mission_state: Option<String>,
+    useless: Option<String>, // This is a hack to allow for empty form submissions
 }
 
-async fn list_data(data_map: DataMap) -> String {
-    let map = data_map.lock().unwrap();
-    let mut data = String::new();
-    for (key, value) in map.iter() {
-        data.push_str(&format!("{}: {}\n", key, value));
+async fn list_all_worker_name() -> String {
+    get_all_worker_names()
+        .iter()
+        .fold("".to_string(), |acc, name| format!("{}{}\n", acc, name))
+}
+
+fn get_all_worker_names() -> Vec<String> {
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "SELECT name FROM name_table";
+    let names: Vec<String> = conn.query_map(query, |name| name).unwrap();
+    names
+}
+
+async fn add_worker_name(Form(isheet): Form<Postsheet>) -> String {
+    if isheet.worker_name.is_none() {
+        return "worker_name not provided".to_string();
     }
-    data
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "INSERT INTO name_table (name) VALUES (:name)";
+    match conn.exec_drop(
+        query,
+        params! { "name" => isheet.worker_name.clone().unwrap() },
+    ) {
+        Ok(_) => "name insert successful".to_string(),
+        Err(_) => "name insert failed".to_string(),
+    }
+}
+
+async fn remove_worker_name(Form(isheet): Form<Postsheet>) -> String {
+    if isheet.worker_name.is_none() {
+        return "worker_name not provided".to_string();
+    }
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    //DELETE FROM `todo_list`.`name_table` WHERE (`name` = 'Sandy');
+    let query = "DELETE FROM name_table WHERE  (`name` =:name)";
+    match conn.exec_drop(
+        query,
+        params! { "name" => isheet.worker_name.clone().unwrap() },
+    ) {
+        Ok(_) => "worker_name remove successful".to_string(),
+        Err(_) => "worker_name remove failed".to_string(),
+    }
+}
+
+async fn add_mission(Form(isheet): Form<Postsheet>) -> String {
+    if isheet.mission_name.is_none() {
+        return "mission_name not provided".to_string();
+    }
+    if isheet.worker_name.is_none() {
+        return "worker_name not provided".to_string();
+    }
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "INSERT INTO mission_table (`mission_name`, `worker_name`, `state`) VALUES (:mission_name,:worker_name,:state)";
+    match conn.exec_drop(
+        query,
+        params! { "mission_name" => isheet.mission_name.clone().unwrap(),
+        "worker_name" => isheet.worker_name.clone().unwrap(),
+        "state" => "nonstart" },
+    ) {
+        Ok(_) => "mission insert successful".to_string(),
+        Err(_) => "mission insert failed".to_string(),
+    }
+}
+
+async fn remove_mission(Form(isheet): Form<Postsheet>) -> String {
+    if isheet.mission_id.is_none() {
+        return "mission_id not provided".to_string();
+    }
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "DELETE FROM mission_table WHERE  (`id` =:mission_id)";
+    match conn.exec_drop(
+        query,
+        params! { "mission_id" => isheet.mission_id.clone().unwrap() },
+    ) {
+        Ok(_) => "mission remove successful".to_string(),
+        Err(_) => "mission remove failed".to_string(),
+    }
+}
+
+async fn list_all_mission(Form(isheet): Form<Postsheet>) -> String {
+    get_all_missions(Form(isheet))
+        .iter()
+        .fold("".to_string(), |acc, mission| {
+            format!("{}{}\n", acc, mission.join(" "))
+        })
+}
+
+fn get_all_missions(Form(isheet): Form<Postsheet>) -> Vec<Vec<String>> {
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "SELECT * FROM mission_table";
+
+    let missions = conn
+        .query_map(query, |n: Row| {
+            vec![
+                n.get("id").unwrap(),
+                n.get("mission_name").unwrap(),
+                n.get("worker_name").unwrap(),
+                n.get("state").unwrap(),
+            ]
+        })
+        .unwrap();
+    if let Some(worker_name) = isheet.worker_name {
+        missions
+            .iter()
+            .filter(|mission| mission[2] == worker_name)
+            .map(|mission| mission.clone())
+            .collect()
+    } else if let Some(mission_name) = isheet.mission_name {
+        missions
+            .iter()
+            .filter(|mission| mission[1] == mission_name)
+            .map(|mission| mission.clone())
+            .collect()
+    } else if let Some(mission_state) = isheet.mission_state {
+        missions
+            .iter()
+            .filter(|mission| mission[3] == mission_state)
+            .map(|mission| mission.clone())
+            .collect()
+    } else if let Some(mission_id) = isheet.mission_id {
+        missions
+            .iter()
+            .filter(|mission| mission[0] == mission_id.to_string())
+            .map(|mission| mission.clone())
+            .collect()
+    } else {
+        missions
+    }
+}
+
+async fn update_mission_state(Form(isheet): Form<Postsheet>) -> String {
+    if isheet.mission_id.is_none() {
+        return "mission_id not provided".to_string();
+    }
+    if isheet.mission_state.is_none() {
+        return "mission_state not provided".to_string();
+    }
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "UPDATE mission_table SET state = :state WHERE id = :id";
+    match conn.exec_drop(
+        query,
+        params! {
+            "state" => isheet.mission_state.clone().unwrap(),
+            "id" => isheet.mission_id.clone().unwrap(),
+        },
+    ) {
+        Ok(_) => "mission state update successful".to_string(),
+        Err(_) => "mission state update failed".to_string(),
+    }
+}
+
+async fn update_mission_name(Form(isheet): Form<Postsheet>) -> String {
+    if isheet.mission_id.is_none() {
+        return "mission_id not provided".to_string();
+    }
+    if isheet.mission_name.is_none() {
+        return "mission_name not provided".to_string();
+    }
+    let url = "mysql://root:123456@localhost:3306/todo_list";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let query = "UPDATE mission_table SET mission_name = :mission_name WHERE id = :id";
+    match conn.exec_drop(
+        query,
+        params! {
+            "mission_name" => isheet.mission_name.clone().unwrap(),
+            "id" => isheet.mission_id.clone().unwrap(),
+        },
+    ) {
+        Ok(_) => "mission name update successful".to_string(),
+        Err(_) => "mission name update failed".to_string(),
+    }
 }
---
+++ todo_list.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todo_list
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mission_table`
+--
+
+DROP TABLE IF EXISTS `mission_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mission_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mission_name` varchar(45) NOT NULL,
+  `worker_name` varchar(45) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mission_table`
+--
+
+LOCK TABLES `mission_table` WRITE;
+/*!40000 ALTER TABLE `mission_table` DISABLE KEYS */;
+INSERT INTO `mission_table` VALUES (1,'test_rust','Daniel','nonstart'),(2,'測試rust','Daniel','nonstart'),(5,'測試rust','Daniel','nonstart'),(6,'測試rust1111','Daniel','nonstart');
+/*!40000 ALTER TABLE `mission_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `name_table`
+--
+
+DROP TABLE IF EXISTS `name_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `name_table` (
+  `name` char(30) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `name_table`
+--
+
+LOCK TABLES `name_table` WRITE;
+/*!40000 ALTER TABLE `name_table` DISABLE KEYS */;
+INSERT INTO `name_table` VALUES ('Daniel'),('godkiller'),('godkiller1'),('godkiller2'),('Sandy');
+/*!40000 ALTER TABLE `name_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-12 19:34:55
